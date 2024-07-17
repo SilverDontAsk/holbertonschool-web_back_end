605ac3abe9a95e6a54b801adf1d58d97b0f97346
Silver <silverdontask@SilverDontAsk.localdomain>
2024-07-17 12:34:34 -0800

    .

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,10 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const [idx, value] of array.entries()) {
-    array[idx] = appendString + value;
+    let idx = 0;
+    for (const value of array) {
+      array[idx] = appendString + value;
+      idx++;
+    }
+  
+    return array;
   }
-
-  return array;
-}
+  
