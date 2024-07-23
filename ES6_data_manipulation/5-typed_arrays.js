5bc26494b73683d14c824adc94159c745838efc3
Silver <silverdontask@SilverDontAsk.localdomain>
2024-07-23 10:29:14 -0800

    >

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,14 @@
+function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
+
+  if (position < 0 || position >= length) {
+    throw new Error('Position out of range');
+  }
+
+  view.setInt8(position, value);
+
+  return buffer;
+}
+
+export default createInt8TypedArray;
