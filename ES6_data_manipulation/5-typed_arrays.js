83bcda1cc4cd344c07351735780710d7eebf5b5e
Silver <silverdontask@SilverDontAsk.localdomain>
2024-07-23 11:56:31 -0800

    >

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -3,7 +3,7 @@ function createInt8TypedArray(length, position, value) {
   const view = new DataView(buffer);
 
   if (position < 0 || position >= length) {
-    throw new Error('Position out of range');
+    throw new Error('Position outside range');
   }
 
   view.setInt8(position, value);
