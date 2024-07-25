48ebfcdeed69bc025703be893110194ba9fd039e
Silver <silverdontask@SilverDontAsk.localdomain>
2024-07-25 10:51:45 -0800

    >

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,13 +1,15 @@
 function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            const success = true;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const success = true;
 
-            if (success) {
-                resolve("API response data");
-            } else {
-                reject(new Error("API call failed"));
-            }
-        }, 1000);
-    });
+      if (success) {
+        resolve('API response data');
+      } else {
+        reject(new Error('API call failed'));
+      }
+    }, 1000);
+  });
 }
+
+export default getResponseFromAPI;
