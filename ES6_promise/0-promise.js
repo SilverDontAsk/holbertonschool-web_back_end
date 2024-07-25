181643256c6f9bcb0eaf7125c106b9f0c35d1ac9
Silver <silverdontask@SilverDontAsk.localdomain>
2024-07-25 10:48:21 -0800

    >

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,13 @@
+function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const success = true;
+
+            if (success) {
+                resolve("API response data");
+            } else {
+                reject(new Error("API call failed"));
+            }
+        }, 1000);
+    });
+}
