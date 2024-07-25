8163352fde126d112431309b8615fc55d4b1a1e4
Silver <silverdontask@SilverDontAsk.localdomain>
2024-07-25 15:11:04 -0800

    >

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,21 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  try {
-    const signUpPromise = signUpUser(firstName, lastName);
-    const uploadPhotoPromise = uploadPhoto(fileName);
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
 
-    const results = await Promise.allSettled([signUpPromise, uploadPhotoPromise]);
-
-    return results.map((result) => ({
+  return Promise.allSettled([signUpPromise, uploadPhotoPromise])
+    .then((results) => results.map((result) => ({
       status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    }));
-  } catch (error) {
-    return [{
-      status: 'rejected',
-      value: error,
-    }];
-  }
+      value: result.status === 'fulfilled' ? result.value : [result.reason],
+    })));
 }
