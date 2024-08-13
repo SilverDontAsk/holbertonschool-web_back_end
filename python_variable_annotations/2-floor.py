64841b48976cd1194a54195909d3ec015ee2de7d
Silver <silverdontask@SilverDontAsk.localdomain>
2024-08-13 10:35:21 -0800

    I dont have a team for the demo day things yet but i'll do this for now cause I genuinely dont know what to do aside from this

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import math
+"""
+This function just returns the floor of a float
+"""
+
+
+def floor(n: float) -> int:
+    """
+    This function uses the math module function of
+    floor to calculate the floor of float n
+    Args:
+        n: expected to be a float
+    Return:
+        Floor of n
+    """
+    return math.floor(n)
