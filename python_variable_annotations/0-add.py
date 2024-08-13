e56348f91d79250eebe2c21b536336a016d137ea
Silver <silverdontask@SilverDontAsk.localdomain>
2024-08-13 09:58:01 -0800

    readme and first exercise

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def add(a: float, b: float) -> float:
+    """
+    This function adds two floats
+    
+    Args: 
+        a: Expected to be a float 
+        b: Expected to be a float
+    
+    Returns:
+        The sum of floats a and b
+    
+    """
     return a + b
