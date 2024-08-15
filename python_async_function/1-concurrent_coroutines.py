3d0733e7b1220a968f66ee8ad19fed1ca3ab59f0
Silver <silverdontask@SilverDontAsk.localdomain>
2024-08-15 12:36:21 -0800

    >

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -3,10 +3,11 @@
 Concurrent coroutines
 """
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, max_delay):
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     This function returns a list of all delays
     Args:
@@ -15,8 +16,13 @@ async def wait_n(n, max_delay):
     Returns:
         list of all delays
     """
+    tasks = [await wait_random(max_delay) for _ in range(n)]
+    
     delay = []
-    tasks = [wait_random(max_delay) for _ in range(n)]
-    for task in asyncio.as_completed(tasks):
-        delay.append(await task)
+    for task in tasks:
+        delay.append(task)
+        for i in range(len(delay) -1, 0, -1):
+            if delay[i] < delay[i - 1]:
+                delay[i], delay[i - 1] = delay[i - 1], delay[i]
     return delay
+    
