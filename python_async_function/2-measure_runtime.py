1729aaf5bbb1a0a4b3134bd2d67b08e25f47788f
Silver <silverdontask@SilverDontAsk.localdomain>
2024-08-15 13:19:26 -0800

    >

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -3,10 +3,11 @@
 Measuring runtime
 """
 import time
+import asyncio
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """
     Measures the runtime for wait_n
     Args:
@@ -16,6 +17,6 @@ async def measure_time(n: int, max_delay: int) -> float:
         Average runtime per task
     """
     start_clock = time.time()
-    await wait_n(n, max_delay)
+    asyncio.run(wait_n(n, max_delay))
     total_time = time.time() - start_clock
     return total_time / n
