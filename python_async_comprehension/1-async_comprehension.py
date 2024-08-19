4fac6a1216369b499f9b663663b501fc838286da
Silver <silverdontask@SilverDontAsk.localdomain>
2024-08-19 13:48:58 -0800

    >

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -2,9 +2,8 @@
 """
 Async comprehension that collects 10 random floats
 """
-async_generator = __import__('0-async_generator').async_generator
-import asyncio
 from typing import List
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
