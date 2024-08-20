d956d6bf43bb4d6ea20b8940c1b79b91f8f21038
Silver <silverdontask@SilverDontAsk.localdomain>
2024-08-19 16:49:40 -0800

    I was complicating the problem by calling for async_comprehension 4 times instead of doing a for method to do it 4 times for me

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -4,7 +4,6 @@ Measuring the runtime of parellel executions
 """
 import asyncio
 import time
-from typing import List
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
@@ -16,11 +15,7 @@ async def measure_runtime() -> float:
         float: total runtime
     """
     start_time = time.time()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
+    progress = [async_comprehension() for _ in range(4)]
+    await asyncio.gather(*progress)
     end_time = time.time()
     return end_time - start_time
