a0efc692105d9cb6e5c4537bf5adc976c04b0c72
Silver <silverdontask@SilverDontAsk.localdomain>
2024-08-22 14:27:41 -0800

    doing python scripts

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -2,24 +2,26 @@
 """
 Providing stats about Nginx logs
 """
+from pymongo import MongoClient
 
 
-def Nginx_log_provider(mongo_collection):
+def Nginx_log_provider():
     """
     Function to provide the nginx logs stored in Mongodb
-    Args:
-        mongo_collection: pymongo collection object
-    Returns:
-        Log stats
     """
-    all_logs = mongo_collection.count_documents({})
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_coll = client.logs.nginx
+    all_logs = nginx_coll.count_documents({})
     print(f"{all_logs} logs")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print("Methods:")
     for method in methods:
-        count = mongo_collection.count_documents({'method': method})
+        count = nginx_coll.count_documents({'method': method})
         print(f"\tmethod {method}: {count}")
-    stat_check_count = mongo_collection.count_documents({
+    stat_check_count = nginx_coll.count_documents({
         'method': 'GET', 'path': '/status'
     })
     print(f"{stat_check_count} status check")
+
+if __name__ == "__main__":
+    Nginx_log_provider()
