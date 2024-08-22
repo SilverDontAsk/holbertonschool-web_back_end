0b8cec16cdd343e9ddb56144ec74ec5b7ba23ae2
Silver <silverdontask@SilverDontAsk.localdomain>
2024-08-22 13:48:09 -0800

    doing python scripts

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+inserting a doc into a mongoDB collection
+with pymongo
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Function to insert a doc into a collection
+    Args:
+        mongo_collection: pymongo collection object
+        **kwargs: fields in the new doc
+    Returns:
+        str: _id of new doc
+    """
+    res = mongo_collection.insert_one(kwargs)
+    return str(res.inserted_id)
