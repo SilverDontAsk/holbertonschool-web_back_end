9c7c17ac66f668f8930755f7c3f74d9daf433a36
Silver <silverdontask@SilverDontAsk>
2024-09-17 13:57:15 -0400

    pushing task 2

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,8 +4,10 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+  console.log(`Your name is: ${name}`);
+  process.stdin.end()
+});
 
-  process.stdout.write('This important software is now closing\n');
-  process.exit();
+process.stdin.on('end', () => {
+    console.log('This important software is now closing\n');
 });
