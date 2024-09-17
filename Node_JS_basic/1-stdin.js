3b580517691ef050a97a0d7ba17221b6ef28da7d
Silver <silverdontask@SilverDontAsk>
2024-09-17 14:12:37 -0400

    actually pushing task 2

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,7 +4,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
+  console.log(`Your name is: ${name}\n`);
 
   if (!process.stdin.isTTY) {
     console.log("This important software is now closing");
