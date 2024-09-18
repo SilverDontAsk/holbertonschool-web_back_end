30b17228b80f8c3d7ca1b69f38deb567e05b2c5f
Silver <silverdontask@SilverDontAsk>
2024-09-18 16:04:26 -0400

    changing error message

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -4,7 +4,7 @@ function countStudents(path) {
   return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf-8', (err, data) => {
       if (err) {
-        reject(new Error('Could not load the database'));
+        reject(new Error('Cannot load the database'));
         return;
       }
 
