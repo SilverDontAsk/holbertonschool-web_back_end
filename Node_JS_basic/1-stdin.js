5be41991a26271bfffb0b3e68034a10f426816c0
Silver <silverdontask@SilverDontAsk>
2024-09-18 14:53:15 -0400

    This SHOULD be good now, outputs are fine so surely no issues

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,12 @@
-// stdin process
-
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
+process.stdin.on('data', (input) => {
+  const name = input.toString().trim();
+
   console.log(`Your name is: ${name}`);
 
   if (!process.stdin.isTTY) {
     console.log('This important software is now closing');
   }
-
   process.exit();
 });
